02c9fca14dd4e53cc3019092a2cfb874e4b279d5
Witkowski, Adam <adam.witkowski@capgemini.com>
2021-06-19 12:34:35 +0200

    Core: Add BudgeterDbContext

---
+++ Budgeter/Data/BudgeterDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Budgeter.Core.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Budgeter.Core.Data
+{
+    public class BudgeterDbContext : DbContext
+    {
+        public DbSet<Cashflow> Cashflows { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Source> Sources { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite(@"Data Source=budgeter.db");
+        }
+    }
+}
