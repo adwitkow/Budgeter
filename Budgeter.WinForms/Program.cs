3e89063b7226ea3b25f1f930691d2fd7e8a6a246
Witkowski, Adam <adam.witkowski@capgemini.com>
2021-06-19 11:41:36 +0200

    Recreate the WinForms project as .NET Core

--- Budgeter.WinForms/Program.cs
+++ Budgeter.WinForms/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Budgeter.WinForms
@@ -9,14 +12,15 @@ namespace Budgeter.WinForms
         private static IServiceProvider ServiceProvider { get; set; }
 
         /// <summary>
-        /// The main entry point for the application.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            
+
             ConfigureServices();
 
             var form = ServiceProvider.GetService<MainForm>();
