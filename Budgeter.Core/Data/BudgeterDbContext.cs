924a156aae51a968ad78b40ee48767f9be40147a
adwitkow <adamadik@poczta.fm>
2021-06-19 16:26:51 +0200

    Remove and sort usings

--- Budgeter.Core/Data/BudgeterDbContext.cs
+++ Budgeter.Core/Data/BudgeterDbContext.cs
@@ -1,8 +1,5 @@
 ﻿using Budgeter.Core.Data.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Budgeter.Core.Data
 {
--- Budgeter.WinForms/Program.cs
+++ Budgeter.WinForms/Program.cs
@@ -1,9 +1,6 @@
 ﻿using Budgeter.Core.Data;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Budgeter.WinForms
