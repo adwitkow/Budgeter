00bc66477e1fb82e26e334c6f2115dd0f9d9d8ef
adwitkow <adamadik@poczta.fm>
2021-06-20 14:17:33 +0200

    Remove and sort usings

--- Budgeter.WinForms/MainForm.cs
+++ Budgeter.WinForms/MainForm.cs
@@ -15,7 +15,6 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 using System.Windows.Forms;
-using Budgeter.DataAccess;
 using Budgeter.ViewModel;
 
 namespace Budgeter.WinForms
