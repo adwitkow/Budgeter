000283c43e0585901facb0a8276bf28513d190a4
adwitkow <adamadik@poczta.fm>
2021-06-20 13:26:02 +0200

    Remove and sort usings

--- Budgeter.WinForms/MainForm.cs
+++ Budgeter.WinForms/MainForm.cs
@@ -14,9 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-using Budgeter.DataAccess;
-using System;
 using System.Windows.Forms;
+using Budgeter.DataAccess;
 
 namespace Budgeter.WinForms
 {
